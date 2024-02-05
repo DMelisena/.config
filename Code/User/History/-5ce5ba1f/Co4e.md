53f8e30ea8b093baa0eed717d6d2626bb8db6ffa
DMelisena <65994685+DMelisena@users.noreply.github.com>
2024-02-05 21:03:06 +0700

    Initial config file upload

---
+++ Code/User/History/-5ce5ba1f/Co4e.md
@@ -0,0 +1,72 @@
+To Do List :
+Make a lateral reading for LINAC callibration
+- Make a slice on x and y on a certain LINAC distancee
+#Directory Guide:
+- Perhitungan Analitik
+    - Primer
+    - Sekunder
+    - Pintu
+- Simulasi OPENMC
+    - CellRSHS (Listing final deteksi rotasi linac)
+        - cellRSHSFIX.py
+        - doseplotcell.py
+    - Kalibrasi (600 MU/min to uSv/h pada dmax water phantom)
+        - kalibrasiSimpel
+            - 1kalibrasiSimpel.py
+            - reader.py
+-   - hasilServer (Output dari running server)
+        -Kalibrasi
+            -reader.py
+        -270deg
+            -doseplotcell.py
+
+# SkripsiL
+Perhitungan analitik dan openmc untuk skripsi
+
+Todolist:
+-Ngetes kode cell filter dose
+    -Masih kurang paham bentuk output openmc
+-Post Processing Cell Filter
+-Pembandingan dengan nilai deteksi
+-Pembuatan formula laju dosis analitik
+-Pembandingan hasil perhitungan laju dosis dengan nilai deteksi
+-Penulisan pembahasan untuk simulasi dan perhitungan
+
+File Tree Directory :
+linac  -> File JIH
+openmc -> File Eksperimental
+rshs   -> Simulasi RSHS
+Program untuk NCRP no.151:
+RSHSPrimer.py
+RSHSSekunder.py
+auto2darray.py
+RSHSPintu.py
+
+
+########## Update 300723
+Hasil Dosis nya aneh, beda jauh. Sepertinya masalah di konversi flux ke dose rate. Harus buat 
+branch baru :
+1. Bikin Tally di tengah (Buat tally grid juga) V
+2. Buat Post Processing
+3. Cek Dose ratenya sesuai dari ICRP atau tidak 
+4. Run simulation 3mil particles 2 batch (Simpan Hasil)
+5. Bandngkan dengan dosis analitik
+6. Konsul ke Mbak Oksel soal s_rate
+BUAT : cek dosis analitik
+Haruskah buat fungsi untuk nentuin sumbu x?
+
+########## Update 270723
+Simulasi sudah benar, tapi belum dicoba untuk cell tally. Konversi flux ke dose rate sudah dilakukan.
+
+########## Update 280623 ##########
+Saat ini nilai2 yang dimasukkan ke berkasnya sudah menghasilkan hasil yang benar
+Should I just make it load an external txt file instead so i would be able to make it automatically convert the value on the text?
+To make it fully automated, what i need to consider is whether it's plausible to have a conceivable input method.
+What are the variables and what are the repeatable constant.
+Var   : 
+W = The number of patient and Gy could be varied 
+U = The number of use factor, I still don't understand it yet
+T = Occupancy Factor tergantung dengan ruangan di sekitar, jadi tergantung
+Const : 
+F = Seharusnya sekitar 41^2 cm kan? gatau sih jenis linac lain gimana
+a = Dari regresi linear
---
+++ Code/User/History/251dca2e/EmW4.py
@@ -0,0 +1,115 @@
+import math
+from math import *
+from tabulate import tabulate
+import numpy as np
+import pandas as pd
+
+#========Beban Kerja
+pasienperhari=70
+gyperpasien=4
+hariperminggu=5
+
+#======== Variabel Asumsi Konstan
+W1=hariperminggu*gyperpasien*pasienperhari #dalam Sv
+print("W = ",W1)
+U=0.33 #karena dipakai 8 jam/24 jam??
+#Tf=1
+
+#==========Pembatas Dosis=====
+brp=20/2/50/1000 #Batas Radiasi Pekerja
+brm=1/2/50/1000 #Batas Radiasi Masyarakat 1mSv*setengah/(50 minggu/tahun)
+
+
+#==========Variabel TVL======
+TVL=0.389
+HVL=TVL*log10(2)
+print("HVL =", HVL)
+TVL1=0.410 ; TVLe=0.370 #mm
+#========Instantenous Dose Rate=====
+DR=2200*60/100 #2200MU/min *jam/menit /100Gy/(MU=cGy)
+#Apakah MU pada linac itu maksimalnya juga 2200?
+
+arrdsad=["d+SAD"]
+arrwb=["Weekly B"]
+arrwn=["Weekly n"]
+arrwsh=["Weekly \nShield"]
+arrwshvl=["+2HVL"]
+arrib=["Inst B"]
+arrin=["Inst n"]
+arrish=["inst \nShield"]
+
+
+
+def rounde(unrounded):
+    roundede="{:.4e}".format(unrounded)
+    return roundede
+
+def WeekDR( W , P , dsad , T ): #W dan U dah ada, dsad ini di ncrp dpri, jarak dari xray target ke titik perlindungan, kok rasanya beda ama dsad yang kuitung ya?
+    arrdsad.append(dsad)
+    b=(P * dsad **2 ) / (W *U *T )
+    arrwb.append("%.5f"%b)
+    n=-log10(b)
+    arrwn.append("%.5f"%n)
+    #return n*TVL #return TVL1+(n-1)TVLe
+    sh=TVL1+(TVLe*(n-1))
+    arrwsh.append("%.5f"%sh)
+    shhvl=sh+2*HVL
+    arrwshvl.append("%.5f"%shhvl)
+    print("==================================================================")
+    print("$$B_{pri}=\\frac{",P," \\times",dsad,"^{2}}{",W,"\\times",U,"\\times",T,"} =",rounde(b),"$$")
+    print("$$n=-log(",rounde(b),")=",rounde(n),"$$")
+    print ("$$t_{barrier}=(",TVL1,"+(",rounde(n),"-1)\\times",TVLe,"=",rounde(sh),"$$")
+    return sh
+
+
+def InstDR(P,dsad):
+    b=(P/40/1000)*(dsad**2)/DR #Dibagi 1000 karena dirubah dari mSv ke Sv
+    arrib.append(b)
+    n=-log10(b)
+    arrin.append("%.5f"%n)
+    #return n*TVL 
+    sh =TVL1+(TVLe*(n-1)) 
+    arrish.append("%.5f"%sh)
+    print("==================================================================")
+    print("$$B_{IDR}=\\frac {",P,"(",dsad,"^{2})}{",DR,"}=",rounde(b),"$$") #tinggal dihapus jarak \ dan f jadi langsung latex, formatting issues
+    print("$$n=-log(",rounde(b),")=",rounde(n),"$$")
+    print ("$$t_{barrier}=(",TVL1,"+(",rounde(n),"-1)\\times",TVLe,"=",rounde(sh),"$$")
+    return sh
+def primer(P,dsad,T):
+    WeekDR(P,dsad,T)
+    InstDR(P,dsad)
+
+###### Nilai Input ###################
+######################################
+primer(1400,brp,7.32,1)  ##################
+primer(1400,brp,7.32,1)  ##################
+primer(3488,brp,7.62,0.5)
+######################################
+
+
+
+array=[]
+array.append(arrdsad)
+array.append(arrwb)
+array.append(arrwn)
+array.append(arrwsh)
+array.append(arrwshvl)
+array.append(arrib)
+array.append(arrin)
+array.append(arrish)
+
+nparray=np.array(array)
+obarray=np.array(nparray,dtype=object)
+tarray=obarray.T
+trarray=np.transpose(obarray)
+print("Data type:", trarray.dtype)
+print(tabulate(trarray,tablefmt="grid"))
+
+#================ Pencetakan Tabel pada .csv ================
+df = pd.DataFrame(trarray)
+
+# Export the DataFrame to a CSV file
+df.to_csv('hasilPrimer.csv', index=False, header=False)
+
+P1=0.0002*(10**4.7)
+print(f'ekspektasi dosis = {P1}')
